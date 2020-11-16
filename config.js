1a823c5161b88754a459f426b691a7d25c8bb6c8
shamim.shahraeeni <shamim.shahraeeni@partsoftware.com>
2020-11-16 09:52:01 +0330

    initial commit

--- config.js
+++ config.js
@@ -7,8 +7,6 @@ const serverRequirement = {
     hostname: 'localhost',
     port: 6379, 
     database: 'mu_local_redis_db',
-    password: "132940547681",
-    username: "ShamimShahraeini",
     ssl: true
   }
   
